ac6b8965d674f17590657500c88c086f753bba6e
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2019-11-22 18:58:05 -0300

    first version of the app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, Loading, AlertController } from 'ionic-angular';
+import { CheckinService } from '../../services/CheckinService';
+import { Checkin } from '../../models/Checkin';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+    checkin: Checkin = new Checkin();
 
-  constructor(public navCtrl: NavController) {
+    constructor(
+      public navCtrl: NavController, 
+      public loadCtrl: LoadingController,
+      public alertCtrl: AlertController,
+      public _checkinService: CheckinService
+    ) {}
+    
+    doCheckin(form) {
+      console.log(form.value)
+      let loading: Loading = this._showLoading();
 
-  }
+      this._checkinService.checkin(this.checkin)
+        .subscribe(response => {
+          loading.dismiss();          
+          console.log(response);
+          this._showAlert(response)
+        }, error => {
+          console.log(error);
+          this._showAlert(error)
+        });
+    }  
 
+    private _showLoading(): Loading {
+      let loading: Loading = this.loadCtrl.create({
+        content: 'Please wait...'
+      });
+  
+      loading.present();
+  
+      return loading;
+    }
+  
+    private _showAlert(message: any): void {
+      this.alertCtrl.create({
+        message: message,
+        buttons: ['Ok']
+      }).present();
+    }
 }
---
+++ src/util/Constant.ts
@@ -0,0 +1,5 @@
+export class Constants {
+    public static API_HOST:string = "https://jugvale-checkin-api.herokuapp.com/api/v1/";
+
+    constructor(){}
+}
